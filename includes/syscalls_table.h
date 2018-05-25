0ad3f75b811fa5cce81d374321155253a3510481
ndudnicz <ndudnicz@protonmail.com>
2018-05-25 17:41:25 +0200

    update syscalls table

--- includes/syscalls_table.h
+++ includes/syscalls_table.h
@@ -1,9 +1,7 @@
 #ifndef SYSCALLS_TABLE_H
 # define SYSCALLS_TABLE_H
 
-# define SYSCALL_NUMBER		329
-
-# define MODE_SPECIAL		0x01
+# define SYSCALL_NUMBER		333
 
 enum e_param {
 	E_NONE = 0,
@@ -18,8 +16,6 @@ typedef struct		s_syscall {
 	char const *const		name;
 	int const				n_param;
 	enum e_param const		params[6];
-	unsigned char const		mode;
-	int const				n_param_p1;
 }					t_syscall;
 
 extern t_syscall const syscalls_table[SYSCALL_NUMBER];
